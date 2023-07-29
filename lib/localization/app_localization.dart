7d936901b8a843f2bc6365c3eb465a0d9004642d
LowkeyGud <foreducationalonlytbh@gmail.com>
2023-07-29 18:55:01 +0545

    Added 9 new languages support

--- lib/localization/app_localization.dart
+++ lib/localization/app_localization.dart
@@ -1,9 +1,30 @@
 import 'package:get/get.dart';
-import 'package:habittracker/localization/chinese/chinese_translations.dart';
-import 'package:habittracker/localization/en_us/en_us_translations.dart';
-import 'package:habittracker/localization/nepali/nepali_translations.dart';
+import 'package:habittracker/localization/languages/arabic_translations.dart';
+import 'package:habittracker/localization/languages/bengali_translations.dart';
+import 'package:habittracker/localization/languages/chinese_translations.dart';
+import 'package:habittracker/localization/languages/deutsch_translations.dart';
+import 'package:habittracker/localization/languages/en_us_translations.dart';
+import 'package:habittracker/localization/languages/hindi_translations.dart';
+import 'package:habittracker/localization/languages/japanese_translations.dart';
+import 'package:habittracker/localization/languages/nepali_translations.dart';
+import 'package:habittracker/localization/languages/portuguese_translations.dart';
+import 'package:habittracker/localization/languages/russian_translations.dart';
+
+import 'languages/espanol_translations.dart';
 
 class AppLocalization extends Translations {
   @override
-  Map<String, Map<String, String>> get keys => {'en_US': enUs, 'ne_Ne': neNe, 'ch_simplified': chCh};
+  Map<String, Map<String, String>> get keys => {
+        'English': enUs,
+        'नेपाली': neNe,
+        '中国人': chCh,
+        'Português': ptBr,
+        'Español': espanolTranslations,
+        'Deutsch': deutschTranslations,
+        'Русский': russianTranslations,
+        'हिन्दी': enHindi,
+        '日本語': japaneseTranslations,
+        'العربية': arabicTranslations,
+        'বাংলা': bengaliTranslations,
+      };
 }
